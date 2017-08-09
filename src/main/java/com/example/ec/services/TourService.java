f8bd0024af12e4cad883641b899cb14146919582
Dabisa Kelava <dabisa.kelava@gmail.com>
2017-08-09 03:15:20 +0100

    added services

---
+++ src/main/java/com/example/ec/services/TourService.java
@@ -0,0 +1,37 @@
+package com.example.ec.services;
+
+import com.example.ec.domain.Difficulty;
+import com.example.ec.domain.Region;
+import com.example.ec.domain.Tour;
+import com.example.ec.domain.TourPackage;
+import com.example.ec.repo.TourPackageRepository;
+import com.example.ec.repo.TourRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TourService {
+    TourPackageRepository tourPackageRepository;
+    TourRepository tourRepository;
+
+    @Autowired
+    public TourService(TourPackageRepository tourPackageRepository, TourRepository tourRepository) {
+        this.tourPackageRepository = tourPackageRepository;
+        this.tourRepository = tourRepository;
+    }
+
+    public Tour createTour(String title, String description, String blurb, Integer price, String duration, String bullets,
+                           String keywords, String tourPackageName, Difficulty difficulty, Region region) {
+        TourPackage tourPackage = tourPackageRepository.findByName(tourPackageName);
+        if(tourPackage == null) {
+            throw new RuntimeException("Tour Package does not exist: " + tourPackageName);
+        }
+        return tourRepository.save(new Tour(title, description, blurb, price, duration, bullets, keywords, tourPackage, difficulty, region));
+    }
+    public Iterable<Tour> lookup() {
+        return tourRepository.findAll();
+    }
+    public long count() {
+        return tourRepository.count();
+    }
+}
